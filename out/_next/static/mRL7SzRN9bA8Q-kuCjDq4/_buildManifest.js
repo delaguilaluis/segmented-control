0c5705ea9009f99e3f5b70e9cbc0d1414ff22332
Luis Del Águila <luisdelaguila@outlook.com>
2023-06-21 13:54:07 +0200

    Deploy

---
+++ out/_next/static/mRL7SzRN9bA8Q-kuCjDq4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/798-8606d3f669c0c564.js","static/chunks/pages/index-fa926ebfdcda0f9b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
